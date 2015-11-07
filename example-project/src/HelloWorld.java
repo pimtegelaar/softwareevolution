08420e985af532a22e0f0c0dfff77ed594213260
pimtegelaar <pimtegelaar@gmail.com>
2015-11-07 22:46:54 +0100

    Add comments for testing

--- example-project/src/HelloWorld.java
+++ example-project/src/HelloWorld.java
@@ -1,8 +1,26 @@
 
 public class HelloWorld {
 
+	/**
+	 * My main method
+	 * This is also a multi-line comment.
+	 * @param args some arguments
+	 */
 	public static void main(String[] args) {
+		// A single line comment
 		System.out.println("Hello World!");
+		/* A one line comment*/
+		/** Another one line comment*/
+		
+		
+		/* The following statement is split up over two lines
+		 *  (note that the second line starts with a '*')
+		 */
+		int nine = 3
+				* 3;
+		
+		System.out.println(nine);		
+		System.out.println("Bye // Bye");		
 	}
 
 }
