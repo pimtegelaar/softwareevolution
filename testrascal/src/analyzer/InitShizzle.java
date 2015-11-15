a4b8e267c78ebd267ad6c84e6e84cc8265572e9b
softwaregast <weisflog@hotmail.com>
2015-11-15 13:25:38 +0100

    Additional test

--- testrascal/src/analyzer/InitShizzle.java
+++ testrascal/src/analyzer/InitShizzle.java
@@ -3,6 +3,9 @@ package analyzer;
 public class InitShizzle {
 	
 	public int doInitializeShizzle(String what) {
+		int doingTheSameThing;
+		
+		doingTheSameThing = 1;
 		if (what == "hoi")
 			return 1;
 		else
