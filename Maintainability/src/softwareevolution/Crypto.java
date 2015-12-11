752a93fe87758b6f4982a9da7fede8a8665601df
pimtegelaar <pimtegelaar@gmail.com>
2015-12-11 17:33:01 +0100

    added cryptography module

---
+++ Maintainability/src/softwareevolution/Crypto.java
@@ -0,0 +1,31 @@
+package softwareevolution;
+
+import java.io.UnsupportedEncodingException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+import org.rascalmpl.value.IString;
+import org.rascalmpl.value.IValueFactory;
+
+public class Crypto {
+  
+  private final IValueFactory vf;
+
+  public Crypto(IValueFactory vf) {
+    this.vf = vf;
+  }
+  
+  public IString digest(IString input, IString algorithm) throws NoSuchAlgorithmException, UnsupportedEncodingException {
+    return digest(input, vf.string("UTF-8"),algorithm);
+  }
+  
+  public IString digest(IString input, IString encoding, IString algorithm) throws NoSuchAlgorithmException, UnsupportedEncodingException {
+    return vf.string(digest(input.getValue(), encoding.getValue(), algorithm.getValue()));
+  }
+  
+  private String digest(String input, String encoding, String algorithm) throws NoSuchAlgorithmException, UnsupportedEncodingException {
+    MessageDigest md = MessageDigest.getInstance(algorithm);
+    byte[] digest = md.digest(input.getBytes(encoding));
+    return new String(digest);
+  }
+}
---
+++ Maintainability/src/softwareevolution/Crypto.rsc
@@ -0,0 +1,13 @@
+module softwareevolution::Crypto
+
+public str md5(str input) =  md5(input,"UTF-8");
+public str md5(str input, str encoding) = digest(input,encoding,"MD5");
+
+public str sha1(str input) =  sha1(input,"UTF-8");
+public str sha1(str input, str encoding) = digest(input,encoding,"SHA1");
+
+@javaClass{softwareevolution.Crypto}
+public java str digest(str input, str algorithm);
+
+@javaClass{softwareevolution.Crypto}
+public java str digest(str input, str encoding, str algorithm);
