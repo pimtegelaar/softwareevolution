3935a34f1b08f4e6a08f6c4d88a5e9addead9e14
pimtegelaar <pimtegelaar@gmail.com>
2015-12-14 09:21:03 +0100

    Add duplication in testcode

--- testrascal/src/analyzer/InitShizzle.java
+++ testrascal/src/analyzer/InitShizzle.java
@@ -11,5 +11,25 @@ public class InitShizzle {
 		else
 			return 0;
 	}
+	
+	public int doInitializeShizzle2(String what) {
+		int doingTheSameThingWithADifferentName;
+		
+		doingTheSameThingWithADifferentName = 1;
+		if (what == "hoi")
+			return 1;
+		else
+			return 0;
+	}
+	
+	public int doInitializeShizzle3(String what) {
+		int doingTheSameThing;
+		
+		doingTheSameThing = 1;
+		if (what == "hoi")
+			return 1;
+		else
+			return 0;
+	}
 
 }
